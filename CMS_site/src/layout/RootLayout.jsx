2da67e3559b284c0cd83bdb7b0e738ac25fd04ad
gabrielavania <gabriela.vania98@gmail.com>
2025-06-22 14:57:02 +0700

    fix: navbar

--- CMS_site/src/layout/RootLayout.jsx
+++ CMS_site/src/layout/RootLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import NavBar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 export default function RootLayout() {
